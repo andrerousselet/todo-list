132bbc3306b42261f1fe877372fe8229630462bd
Andre Rousselet <andre098@hotmail.com>
2023-06-07 19:21:18 -0300

    feat(form): add todo form containing input and add button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,21 @@
-import { Text, View } from "react-native";
+import { TextInput, View, TouchableOpacity, Text } from "react-native";
 import { styles } from "./styles";
 
-export function Home() {}
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.todoForm}>
+        <TextInput
+          style={styles.todoInput}
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor="#808080"
+        />
+        <TouchableOpacity style={styles.todoAddButton}>
+          <View style={styles.circle}>
+            <Text style={styles.plus}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,6 +2,49 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: "#1A1A1A",
+    flex: 1,
+    paddingHorizontal: 24
   },
+  todoForm: {
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 4,
+    justifyContent: "center",
+    width: "100%",
+    marginTop: -27,
+  },
+  todoInput: {
+    backgroundColor: "#262626",
+    borderRadius: 6,
+    color: "#F2F2F2",
+    flex: 1,
+    fontSize: 16,
+    height: 54,
+    padding: 16,
+  },
+  todoAddButton: {
+    alignItems: "center",
+    backgroundColor: "#1E6F9F",
+    borderRadius: 6,
+    height: 54,
+    justifyContent: "center",
+    width: 54,
+  },
+  circle: {
+    alignItems: "center",
+    backgroundColor: "#1E6F9F",
+    borderColor: "#F2F2F2",
+    borderRadius: 100,
+    borderWidth: 1,
+    height: 16,
+    justifyContent: "center",
+    width: 16,
+  },
+  plus: {
+    color: "#F2F2F2",
+    fontSize: 16,
+    lineHeight: 16,
+    textAlign: "center",
+  }
 });
